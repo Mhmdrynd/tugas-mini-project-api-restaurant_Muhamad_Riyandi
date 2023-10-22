316c9cb8f441d7c9702e92a873c6d4c97d7ece8a
Muhamad Riyandi <72942758+Mhmdrynd@users.noreply.github.com>
2023-10-22 13:45:30 +0700

    Add files via upload

--- db/config.js
+++ db/config.js
@@ -9,6 +9,8 @@ db.serialize(() => {
         updated_at DATETIME default CURRENT_TIMESTAMP);`)
     db.run(`CREATE TABLE IF NOT EXISTS categories (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT,created_at DATETIME default CURRENT_TIMESTAMP,
         updated_at DATETIME default CURRENT_TIMESTAMP);`)
+        db.run(`CREATE TABLE IF NOT EXISTS orders (id INTEGER PRIMARY KEY AUTOINCREMENT, custumer_id INTEGER, menu_id INTEGER, qty INTEGER, order_date DATE, created_at DATETIME DEFAULT CURRENT_TIMESTAMP, update_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (customer_id) REFERENCES custumer(id), FOREIGN KEY (menu_id) REFERENCES menu(id));`)
 })
 db.on('open',() => console.log("Success generate table !"))
 db.on('error',() => console.log("database error!"))
